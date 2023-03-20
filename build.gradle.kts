2a5cc1d1b34eb43ac6d176a1ab0dcca567c6ed44
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-03-20 10:30:10 -0300

    Refactor project, add new ico

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,17 @@ plugins {
 
 fun org.jetbrains.kotlin.gradle.plugin.mpp.Executable.windowsResources(rcFileName: String) {
     val taskName = linkTaskName.replaceFirst("link", "windres")
-    val inFile = compilation.defaultSourceSet.resources.sourceDirectories.singleFile.resolve(rcFileName)
+    val inFile = compilation.allKotlinSourceSets.stream().filter {
+            println(">>>>> ${it}")
+            it.name == "mingwX64Main"
+    } .findFirst().get().resources.sourceDirectories.singleFile.resolve(rcFileName)
     val outFile = buildDir.resolve("processedResources/$taskName.res")
 
     val windresTask = tasks.create<Exec>(taskName) {
         inputs.file(inFile)
         outputs.file(outFile)
         commandLine("windres", "--use-temp-file", inFile, /*"-D_${buildType.name}",*/ "-O", "coff", "-o", outFile)
-        dependsOn(compilation.compileKotlinTask)
+        dependsOn(compilation.compileTaskProvider)
     }
 
     linkTask.dependsOn(windresTask)
@@ -34,10 +37,19 @@ kotlin {
                 implementation("com.squareup.okio:okio:$okioVersion")
             }
         }
+        val nativeMain by creating {
+            dependsOn(commonMain.get())
+        }
+        val mingwX64Main by creating {
+            if (System.getProperty("os.name").startsWith("Win", ignoreCase = true)) {
+                nativeMain.dependsOn(this)
+            }
+        }
     }
     mingwX64("native") {
         binaries {
-            executable("launcher", listOf(RELEASE, DEBUG)){
+            executable("knxLauncher", listOf(RELEASE, DEBUG)){
+                baseName = "knx-launcher"
                 windowsResources("${baseName}.rc")
                 println("Executable path: ${outputFile.absolutePath}")
             }
